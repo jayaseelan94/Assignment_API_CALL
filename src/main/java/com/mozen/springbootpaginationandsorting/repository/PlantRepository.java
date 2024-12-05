450f5720dcb02dc3a23d6f417ff0d00df6761c01
jayaseelan <71054167+jayaseelan94@users.noreply.github.com>
2024-12-05 16:08:14 +0800

    Delete src directory

--- src/main/java/com/mozen/springbootpaginationandsorting/repository/PlantRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mozen.springbootpaginationandsorting.repository;
-
-import com.mozen.springbootpaginationandsorting.model.Plant;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PlantRepository extends PagingAndSortingRepository<Plant, Long> {
-}
